57fb7e4d2c9c38c20220ef15a525f4eb92722ce8
Lucas V. R <lvr@s-viva.xyz>
2023-07-19 14:29:08 -0300

    load: add loadTemplatesEmbed

--- src/Ondim/Extra/Loading.hs
+++ src/Ondim/Extra/Loading.hs
@@ -12,7 +12,7 @@ import Data.Map ((!))
 import Ondim
 import Relude.Extra (minimumOn1, toPairs)
 import System.FilePath (splitDirectories, (</>))
-import System.FilePattern (FilePattern)
+import System.FilePattern (FilePattern, matchMany)
 import System.UnionMount
 
 newtype TemplateLoadingError = TemplateLoadingException String
@@ -22,11 +22,26 @@ newtype TemplateLoadingError = TemplateLoadingException String
 data LoadFn where
   LoadFn :: OndimNode a => (FilePath -> LByteString -> Either String a) -> LoadFn
 
+fpToIdentifier :: FilePath -> Text
+fpToIdentifier = toText . intercalate "." . splitDirectories
+
+loadFnToUpdate :: LoadFn -> FilePath -> Text -> LByteString -> OndimState n -> OndimState n
+loadFnToUpdate (LoadFn fn) fp name bs s =
+  s {expansions = insertExpansion name res (expansions s)}
+  where
+    site = fileSite fp
+    throw' = throw . TemplateLoadingException
+    res = templateData' site $ either throw' id $ fn fp bs
+
 data LoadConfig = LoadConfig
   { patterns :: [FilePattern],
     loadFn :: LoadFn
   }
 
+{- | Load templates from a list of directories in descending order of priority,
+and return the inital state and a watcher action that takes a handler to update
+the state when templates get updated on disk.
+-}
 loadTemplatesDynamic ::
   forall m n.
   (MonadLogger m, MonadIO m, MonadUnliftIO m) =>
@@ -44,21 +59,28 @@ loadTemplatesDynamic cfgs places =
       handler chg =
         appEndo . mconcat . coerce . join
           <$> forM (toPairs chg) \(i, chg') ->
-            case cfgMap ! i of
-              LoadFn load ->
-                forM (toPairs chg') \(file, fa) ->
-                  let name = toText $ intercalate "." $ splitDirectories file
-                   in case fa of
-                        Refresh _ ls ->
-                          let dir = snd $ minimumOn1 fst (fst <$> ls)
-                              fp = dir </> file
-                              site = fileSite fp
-                              throw' = throw . TemplateLoadingException
-                              res = templateData' site . either throw' id . load fp
-                           in readFileLBS fp <&> \b s ->
-                                s {expansions = insertExpansion name (res b) (expansions s)}
-                        Delete -> pure \s -> s {expansions = deleteExpansion name (expansions s)}
+            forM (toPairs chg') \(file, fa) ->
+              let name = fpToIdentifier file
+               in case fa of
+                    Refresh _ ls ->
+                      let dir = snd $ minimumOn1 fst (fst <$> ls)
+                          fp = dir </> file
+                       in readFileLBS fp <&> loadFnToUpdate (cfgMap ! i) fp name
+                    Delete -> pure \s -> s {expansions = deleteExpansion name (expansions s)}
    in unionMount sources patts exclude initial handler
 
-loadTemplates :: Monad n => [LoadConfig] -> [FilePath] -> IO (OndimState n)
+{- | Load templates from a list of directories in descending order of priority.
+-}
+loadTemplates :: [LoadConfig] -> [FilePath] -> IO (OndimState n)
 loadTemplates cfgs dirs = fst <$> runNoLoggingT (loadTemplatesDynamic cfgs dirs)
+
+{- | Load templates in pure code from a list of filepaths and bytestrings. Meant
+to be used with the @file-embed@ package.
+-}
+loadTemplatesEmbed :: String -> [LoadConfig] -> [(FilePath, ByteString)] -> OndimState n
+loadTemplatesEmbed prefix cfgs files = foldr go mempty res
+  where
+    patts = [(loadFn c, p) | c <- cfgs, p <- patterns c]
+    fdata = [((toLazy bs, fp), fp) | (fp, bs) <- files]
+    res = matchMany patts fdata
+    go (fn, (bs, fp), _) = loadFnToUpdate fn (prefix </> fp) (fpToIdentifier fp) bs
